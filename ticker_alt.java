634a8ee3b8d7fb3a600847e2e6c6aee601e6e586
Mo <mdk.mail.81245@gmail.com>
2025-06-03 10:09:39 +0200

    Create ticker_alt.java

---
+++ ticker_alt.java
@@ -0,0 +1,78 @@
+public void tick(){
+        /*zeit = zeit + 0.05;
+        for(int i = 0; i < getreide.length; i++){
+            if(MD.beruehrt(getreide[i])){
+                gesammelt = gesammelt + 1;
+                anzeige.setzeInhalt(gesammelt);
+                //getreide[i].entfernen();
+                /*
+                for(int j = 0; j < getreide.length; j++){
+                    if(getreide[i] != getreide[j] ){
+                        //if(MD.beruehrt(getreide[i])){
+
+                        if(getreide[i].beruehrt(getreide[j])){
+                            getreide[i].setzeMittelpunkt(zufallszahlVonBis(-13, 13), zufallszahlVonBis(-8, 8));
+                        }
+                    }
+                    if(getreide[i].beruehrt(MD)){
+                        getreide[i].setzeMittelpunkt(zufallszahlVonBis(-13, 13), zufallszahlVonBis(-8, 8));
+
+                    }
+
+                }
+                
+                ZufallGetreide(i);
+            }
+            
+            for(int j = 0; j < getreide.length; j++){
+                if(getreide[i] != getreide[j] ){
+                    //if(MD.beruehrt(getreide[i])){
+                    /*if(getreide[i].beruehrt(MD)){
+                        gesammelt = 0;
+                        anzeige.setzeInhalt(gesammelt);
+
+                    }
+                    if(getreide[i].beruehrt(getreide[j])){
+                        ZufallGetreide(i);
+                        //getreide[i].setzeMittelpunkt(zufallszahlVonBis(-13, 13), zufallszahlVonBis(-8, 8));
+                    }
+                }
+                if(getreide[i].beruehrt(MD)){
+                    getreide[i].setzeMittelpunkt(zufallszahlVonBis(-13, 13), zufallszahlVonBis(-8, 8));
+                    System.out.println("i: " + i + "getreide zufallgespawn anfang"  + "j: " + j);
+                }
+
+            }
+        }
+
+        /*if(gesammelt == 50){
+        over.setzeSichtbar(true);
+        MD.setzeSichtbar(false);
+
+        }*/
+        /* WICHTIG
+        for(int i = 0; i < mast.length; i++){
+            if(MD.beruehrt(mast[i])){
+                over.setzeSichtbar(true);
+                MD.setzeSichtbar(false);
+                for(int j = 0; j < mast.length; j++){
+                    mast[j].setzeSichtbar(false);
+                }
+                
+                for(int j = 0; j < getreide.length; j++){
+                    getreide[j].setzeSichtbar(false);
+                }
+                
+                //xyz = 1;
+            }
+            //if(xyz == 1){MD.setzeSichtbar(false);}
+        }
+        
+        if(getreide[31].nenneSichtbar() == false && zeit == 10){
+            for(int i = 0;i < (getreide.length - 30); i++){
+                //getreide
+
+            }
+
+        }*/
+    }
